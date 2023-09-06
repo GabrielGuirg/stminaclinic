12878e0b938e67d137390ee9fca5e1de5f9c6b58
GabrielGuirg <93854346+GabrielGuirg@users.noreply.github.com>
2023-09-06 14:38:29 -0400

    Added toast

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,22 +1,30 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
 function Layout() {
-    return (
-      <div className="Layout flex flex-column">
-        <div className="absolute top-0 right-0">
-          <Link to="login" className="p-2"> <Button type="button" label="Login" link /> </Link>
-        </div>
-        <Link to="/" >Home</Link>
-        <Link to="/login" >Login</Link>
-        <Link to="behavioral"> Behavioral</Link>
-        <Link to="dashboard"> Dashboard</Link>
-        <Outlet />
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    // Redirect to the login page
+    navigate('/login');
+  };
+
+  return (
+    <div className="Layout flex flex-column">
+      <div className="absolute top-0 right-0">
+        <Button type="button" label="Login" link onClick={handleLoginClick} />
       </div>
-    );
-  }
-  
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="behavioral">Behavioral</Link>
+      <Link to="dashboard">Dashboard</Link>
+    </div>
+  );
+}
+
 export default Layout;
+
 
 /* This controls the main page UI of the layout */ 
 /* The changes to Link to = makes the port name different each time you change it */
--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function showToastMessage() {
+  toast.success('Successful Login', { position: toast.POSITION.BOTTOM_RIGHT });
+}
+
 function Login() {
-    return (
-      <div className="Login">
-        This Login Page
-      </div>
-    );
-  }
-  
-  export default Login;
+  useEffect(() => {
+    // Trigger the toast when the Login component is mounted
+    showToastMessage();
+  }, []); /* Thought this could help?????? idk i had to search up*/
+
+  return (
+    <div className="Login">
+      This is the Login Page
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default Login;
