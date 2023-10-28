e5e8befdf01ba63e22cf34014247a2a71477c5a9
GabrielGuirg <gabriel.guirguis@gmail.com>
2023-10-28 15:01:14 -0400

    Added onto Events

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -37,3 +37,57 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
+// import React, { useEffect, useState } from 'react';
+// import { Sidebar } from 'primereact/sidebar';
+// import { Button } from 'primereact/button';
+// import { InputText } from 'primereact/inputtext';
+// import { Calendar } from 'primereact/calendar';
+// import { Checkbox } from 'primereact/checkbox';
+// import './Dashboard.css';
+// import { Formik, Form, Field, ErrorMessage } from 'formik'; // Import Formik and related components
+// import { generalStore } from '../stores/generalStore';
+// import AddEvent from './components/AddEvent';
+// import MainSidebar from '../main-sidebar/MainSidebar';
+// import EditEvent from './components/EditEvent';
+
+// function Dashboard() {
+
+//   const events = generalStore((state: any) => state.events);
+//   const deleteEvent = generalStore((state: any) => state.deleteEvent);
+//   const getEvents = generalStore((state: any) => state.getEvents);
+
+//   useEffect(() => {
+//     getEvents();
+//   }, []);
+
+//   const addSideBar = generalStore((state: any) => state.addSideBar);
+  
+//   const openAddEventSidebar = () => {
+//     addSideBar(<AddEvent />);
+//   };
+
+//   const openEditEventSidebar = () => {
+//     addSideBar(<EditEvent />);
+//   };
+
+//   return (
+//     <div className="Dashboard">
+//       <div className="card flex justify-content-center">
+//         {
+//           events.map((e: any) => (
+//             <p key={e.id}>
+//               {e.name} <Button icon="pi pi-trash" onClick={() => deleteEvent(e.id)}>Delete</Button>
+//             </p>
+//           ))
+//         }
+//         <Button icon="pi pi-plus" onClick={openAddEventSidebar} />
+//         <Button icon="pi pi-pencil" onClick={openEditEventSidebar} />
+//       </div>
+//       <MainSidebar />
+//     </div>
+//   );
+// }
+
+// export default Dashboard;
+
