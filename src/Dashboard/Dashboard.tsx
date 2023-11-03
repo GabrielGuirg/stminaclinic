17b29aea8810605fc75630e887d1a8523e38a9d1
GabrielGuirg <gabriel.guirguis@gmail.com>
2023-11-03 14:06:35 -0400

    Created Grid Table for Dashboard

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,21 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Sidebar } from 'primereact/sidebar';
+import React, { useEffect } from 'react';
 import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Calendar } from 'primereact/calendar';
-import { Checkbox } from 'primereact/checkbox';
 import './Dashboard.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik'; // Import Formik and related components
 import { generalStore } from '../stores/generalStore';
 import AddEvent from './components/AddEvent';
 import MainSidebar from '../main-sidebar/MainSidebar';
-import { confirmPopup } from 'primereact/confirmpopup'; // To use confirmPopup method
-import { confirmDialog } from 'primereact/confirmdialog'; // To use confirmDialog method
+import { confirmPopup } from 'primereact/confirmpopup';
 import EditEvent from './components/EditEvent';
-
+import { Card } from 'primereact/card';
+import { Field, Form, Formik } from 'formik';
 
 function Dashboard() {
-
   const events = generalStore((state: any) => state.events);
   const deleteEvent = generalStore((state: any) => state.deleteEvent);
   const getEvents = generalStore((state: any) => state.getEvents);
@@ -24,29 +18,39 @@ function Dashboard() {
     getEvents();
   }, []);
 
-  function deleteEventFunc(event: any, eventId: string) {
+  function deleteEventFunc(EventId: any) {
     confirmPopup({
-      target: event.currentTarget,
       message: 'Are you sure you want to proceed?',
       icon: 'pi pi-exclamation-triangle',
-      accept: () => deleteEvent(eventId),
-  });
+      accept: () => deleteEvent(EventId),
+    });
   }
 
   const addSideBar = generalStore((state: any) => state.addSideBar);
+
   return (
     <div className="Dashboard">
-      {}
       <div className="card flex justify-content-center">
-        {
-          events.map((e: any) => <p key={e.id}>{e.date.toDateString()} <Button onClick={($event) => deleteEventFunc($event, e.id)} >Delete</Button> </p>)
-        }
-        <Button icon="pi pi-plus" onClick={() => {
-          addSideBar(<AddEvent />);
-        }} />
+        {events.map((e:any) => (
+          <div key={e.id}>
+            <p>{e.date.toDateString()}</p>
+            <Button
+              icon="pi pi-trash"
+              onClick={() => deleteEventFunc(e.id)}
+            >
+              Delete
+            </Button>
+          </div>
+        ))}
+        <Button
+          icon="pi pi-plus"
+          onClick={() => {
+            addSideBar(<AddEvent />);
+          }}
+        />
       </div>
       <div>
-        <table>
+      <table className="grid-table">
           <thead>
             <tr>
               <th>Date</th>
@@ -58,19 +62,58 @@ function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            {events.map((e:any)=> <tr key={`event-table-${e.id}`}>
-              <td>{e.date.toDateString()}</td>
-              <td>{e.isBehavioral ? 'Yes': 'No'}</td>
-              <td>{e.desiredAttendance}</td>
-              <td>{e.totalPatient ?? 0}</td>
-              <td>{e.closed}</td>
-              <td>
-                
-        <Button icon="pi pi-plus" onClick={() => {
-          addSideBar(<EditEvent eventId={e.id} desiredAttendence={e.desiredAttendance} isBehavioral={e.isBehavioral} date={e.date} />);
-        }} /> | VIEW
-              </td>
-            </tr>)}
+            {events.map((e: any) => (
+              <tr key={`event-table-${e.id}`}>
+                <td>{e.date.toDateString()}</td>
+                <td>{e.isBehavioral ? 'Yes' : 'No'}</td>
+                <td>{e.desiredAttendance}</td>
+                <td>{e.totalPatient ?? 0}</td>
+                <td>{e.closed}</td>
+                <td>
+                  <Button
+                    icon="pi pi-pencil"
+                    onClick={() => {
+                      addSideBar(
+                        <EditEvent
+                          eventId={e.id}
+                          desiredAttendence={e.desiredAttendance}
+                          isBehavioral={e.isBehavioral}
+                          date={e.date}
+                        />
+                      );
+                    }}
+                  />
+                  <Button
+                    icon="pi pi-user"
+                    onClick={() => {
+                      addSideBar(
+                        <Card title="Title">
+                          <Formik
+                              initialValues={{
+                                eventId: e.id,
+                                firstName: '', // Initialize with values from your database
+                                lastName: '',
+                                phone: '',
+                                email: '',
+                                date: '',
+                                address: '',
+                                city: '',
+                                zipCode: '',
+                                noDoctorRequired: false,
+                                newPatient: false,
+                              }}
+                              onSubmit={(values) => {
+                                // Handle form submission here
+                              }}
+                            >
+                            </Formik>
+                        </Card>
+                      );
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -80,57 +123,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-// import React, { useEffect, useState } from 'react';
-// import { Sidebar } from 'primereact/sidebar';
-// import { Button } from 'primereact/button';
-// import { InputText } from 'primereact/inputtext';
-// import { Calendar } from 'primereact/calendar';
-// import { Checkbox } from 'primereact/checkbox';
-// import './Dashboard.css';
-// import { Formik, Form, Field, ErrorMessage } from 'formik'; // Import Formik and related components
-// import { generalStore } from '../stores/generalStore';
-// import AddEvent from './components/AddEvent';
-// import MainSidebar from '../main-sidebar/MainSidebar';
-// import EditEvent from './components/EditEvent';
-
-// function Dashboard() {
-
-//   const events = generalStore((state: any) => state.events);
-//   const deleteEvent = generalStore((state: any) => state.deleteEvent);
-//   const getEvents = generalStore((state: any) => state.getEvents);
-
-//   useEffect(() => {
-//     getEvents();
-//   }, []);
-
-//   const addSideBar = generalStore((state: any) => state.addSideBar);
-  
-//   const openAddEventSidebar = () => {
-//     addSideBar(<AddEvent />);
-//   };
-
-//   const openEditEventSidebar = () => {
-//     addSideBar(<EditEvent />);
-//   };
-
-//   return (
-//     <div className="Dashboard">
-//       <div className="card flex justify-content-center">
-//         {
-//           events.map((e: any) => (
-//             <p key={e.id}>
-//               {e.name} <Button icon="pi pi-trash" onClick={() => deleteEvent(e.id)}>Delete</Button>
-//             </p>
-//           ))
-//         }
-//         <Button icon="pi pi-plus" onClick={openAddEventSidebar} />
-//         <Button icon="pi pi-pencil" onClick={openEditEventSidebar} />
-//       </div>
-//       <MainSidebar />
-//     </div>
-//   );
-// }
-
-// export default Dashboard;
-
